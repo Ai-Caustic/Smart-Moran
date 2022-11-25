d5230b11c7f7a6f14d9b0fe9785f7602a6cf8311
Victor Micheni <victormichenim@gmail.com>
2022-11-25 18:18:35 +0300

    Updated Requests

--- backend.py
+++ backend.py
@@ -17,7 +17,7 @@ api = Api(app)
 
 class Customer(db.Model):
     __tablename__ = 'customers'
-    customerId = db.Column(db.Integer, primary_key=True)
+    customerId = db.Column(db.Integer, primary_key=True, nullable=False)
     firstName = db.Column(db.String, nullable=False)
     lastName = db.Column(db.String, nullable=False)
     phoneNo = db.Column(db.Integer, nullable=False)
@@ -33,9 +33,6 @@ class User(db.Model):
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
 
-    def __repr__(self):
-        return '<User %s>' % self.firstName
-
 
 # Create a marshmallow schema based on the User model
 class CustomerSchema(ma.Schema):
@@ -45,18 +42,18 @@ class CustomerSchema(ma.Schema):
         model = Customer
 
 
-post_schema = CustomerSchema
-posts_schema = CustomerSchema(many=True)
+customer_schema = CustomerSchema()
+customers_schema = CustomerSchema(many=True)
 
 
 # Create a Restful resource
-class PostListResource(Resource):
+class CustomerListResource(Resource):
     def get(self):
         customers = Customer.query.all()
-        return posts_schema.dump(customers)
+        return customers_schema.dump(customers)
 
     def post(self):
-        new_post = Customer(
+        new_customer = Customer(
             customerId=request.json['customerId'],
             firstName=request.json['firstName'],
             lastName=request.json['lastName'],
@@ -64,19 +61,44 @@ class PostListResource(Resource):
             email=request.json['email'],
             notes=request.json['notes'],
         )
-        db.session.add(new_post)
+        db.session.add(new_customer)
         db.session.commit()
-        return post_schema.dump(new_post)
+        return customer_schema.dump(new_customer)
 
 
-class PostResource(Resource):
-    def get(self, customer_id):
-        customer = Customer.query.get_or_404(customer_id)
-        return post_schema.dump(customer)
+class CustomerResource(Resource):
+    def get(self, customerId):
+        customer = Customer.query.get_or_404(customerId)
+        return customer_schema.dump(customer)
+
+    def patch(self, customerId):
+        customer = Customer.query.get_or_404(customerId)
+
+        if 'customerId' in request.json:
+            customer.customerId = request.json['customerId']
+        if 'firstName' in request.json:
+            customer.firstName = request.json['firstName']
+        if 'lastName' in request.json:
+            customer.lastName = request.json['lastName']
+        if 'phoneNo' in request.json:
+            customer.phoneNo = request.json['phoneNo']
+        if 'email' in request.json:
+            customer.email = request.json['email']
+        if 'notes' in request.json:
+            customer.notes = request.json['notes']
+
+        db.session.commit()
+        return customer_schema.dump(customer)
+
+    def delete(self, customerId):
+        customer = Customer.query.get_or_404(customerId)
+        db.session.delete(customer)
+        db.session.commit()
+        return '', 204
 
 
-api.add_resource(PostListResource, '/customers')
-api.add_resource(PostResource, '/customers/<int:customer_id>')
+api.add_resource(CustomerListResource, '/customers')
+api.add_resource(CustomerResource, '/customers/<int:customerId>')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
